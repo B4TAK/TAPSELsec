c544c533bf26f71ed846f3011b921b9daf061540
Jhosua Saut Maruli <49725122+LuciferXtools@users.noreply.github.com>
2019-07-28 12:55:49 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+pkg install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pip2 install termcolor
+pkg install ruby
+pkg install gem
+gem install lolcat
+pip2 install lolcat
+pkg install openssl
+pkg install php
+pkg install perl
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install figlet
+pkg install nano
